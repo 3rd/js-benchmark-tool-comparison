8dca5f3c5267eca696fdee8ac75dd7c98760e8c0
3rd <3rd@users.noreply.github.com>
2023-06-17 14:30:38 +0300

    test: add mitt & zodbus

--- src/simple/tseep-mitata.mjs
+++ src/simple/tseep-mitata.mjs
@@ -1,12 +1,21 @@
 import * as mitata from "mitata";
+import { z } from "zod";
+import { create } from "zodbus";
 import { EventEmitter } from "tseep";
+import _mitt from "mitt";
 
 function eventHandler(a, b, c, d) {
   if (arguments.length > 100) console.log("check");
 }
 
+const zodbus = create({ schema: { foo: z.string() }, validate: false });
+zodbus.subscribe("foo", eventHandler);
+
 const tseep = new EventEmitter();
 tseep.on("foo", eventHandler);
+
+const mitt = _mitt();
+mitt.on("foo", eventHandler);
 
 mitata.bench("noop", () => {});
 
@@ -16,10 +25,20 @@ mitata.group(() => {
     eventHandler("baz");
     eventHandler("boom");
   });
+  mitata.bench("zodbus publish", function () {
+    zodbus.publish("foo", "bar");
+    zodbus.publish("foo", "baz");
+    zodbus.publish("foo", "boom");
+  });
   mitata.bench("tseep emit", function () {
     tseep.emit("foo", "bar");
     tseep.emit("foo", "baz");
     tseep.emit("foo", "boom");
+  });
+  mitata.bench("mitt emit", function () {
+    mitt.emit("foo", "bar");
+    mitt.emit("foo", "baz");
+    mitt.emit("foo", "boom");
   });
 }, false);
 await mitata.run();
