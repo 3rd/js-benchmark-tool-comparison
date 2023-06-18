e29a79c70fe45575b7d89e078f7c27ea827684d4
3rd <3rd@users.noreply.github.com>
2023-06-18 20:01:11 +0300

    test: add other event buses

--- src/simple/tseep-tinybench.mjs
+++ src/simple/tseep-tinybench.mjs
@@ -1,14 +1,35 @@
 import * as tinybench from "tinybench";
+import { z } from "zod";
+import { create } from "zodbus";
 import { EventEmitter } from "tseep";
+import _mitt from "mitt";
 
 function eventHandler(a, b, c, d) {
   if (arguments.length > 100) console.log("check");
 }
 
+const zodbus = create({ schema: { foo: z.string() }, validate: false });
+zodbus.subscribe("foo", eventHandler);
+
 const tseep = new EventEmitter();
 tseep.on("foo", eventHandler);
 
+const mitt = _mitt();
+mitt.on("foo", eventHandler);
+
 const bench = new tinybench.Bench();
+
+bench.add("direct call", function () {
+  eventHandler("bar");
+  eventHandler("baz");
+  eventHandler("boom");
+});
+
+bench.add("zodbus", function () {
+  zodbus.publish("foo", "bar");
+  zodbus.publish("foo", "baz");
+  zodbus.publish("foo", "boom");
+});
 
 bench.add("tseep", function () {
   tseep.emit("foo", "bar");
@@ -16,10 +37,10 @@ bench.add("tseep", function () {
   tseep.emit("foo", "boom");
 });
 
-bench.add("direct call", function () {
-  eventHandler("bar");
-  eventHandler("baz");
-  eventHandler("boom");
+bench.add("mitt", function () {
+  mitt.emit("foo", "bar");
+  mitt.emit("foo", "baz");
+  mitt.emit("foo", "boom");
 });
 
 await bench.run();
